d1114bd895dbd43d3d2be3713a5bac1c09f567d8
Prospero <109393624+ProsperoAngelo@users.noreply.github.com>
2024-09-17 22:50:10 -0300

    Atualizada a saída de dados
    Foram incluidas as variáveis "nome" e "notas" dentro do laço for. As veriáveis foram chamadas no console.

--- notas-atletas.js
+++ notas-atletas.js
@@ -20,6 +20,10 @@ let atletas = [
 //Percorre cada objeto realizando os cálculos na propriedade notas dos objetos
 for (let i = 0; i < atletas.length; i++) {
 
+      let nome = atletas[i].nome
+
+      let notas = atletas[i].notas
+    
 //Organiza o vetor notas de cada objeto do menor para o maior
   let notasOrganizadas = atletas[i].notas.sort(function(a, b){
     return a - b
@@ -27,19 +31,17 @@ for (let i = 0; i < atletas.length; i++) {
 
 //Separa as 3 notas válidas eliminando a maior e a menor nota índices 1 e 4 do vetor 
   let notasValidas = atletas[i].notas.slice(1,4);
-
+    
 //Soma as notas válidas de cada vetor notas.
   let somaDasNotas = notasValidas.reduce(function (total, atual) {
     return total + atual;
   },0);
 
-
-
   let mediaValida = somaDasNotas/notasValidas.length
 
   console.log("______________________________________")
-  console.log(`Atleta: ${atletas[i].nome}`)
-  console.log(`Notas Obtidas: ${atletas[i].notas}`)
+  console.log(`Atleta: ${nome}`)
+  console.log(`Notas Obtidas: ${notas}`)
 //console.log(`Notas Válidas: ${notasValidas}`)
   console.log(`Média Válida: ${mediaValida}`)
   console.log("--------------------------------------")
